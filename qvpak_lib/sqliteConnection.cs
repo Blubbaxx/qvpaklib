0d9fb37098095f887e81ba59284637dbb6912f54
Blubbaxx <19249053+Blubbaxx@users.noreply.github.com>
2018-02-21 15:16:26 +0100

    Add files via Upload

---
+++ qvpak_lib/sqliteConnection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace qvpak_lib
+{
+    public class sqliteConnection
+    {
+        public string run(string database, string label, string table, string id)
+        {
+            SQLiteConnection connection;
+
+            string read = "";
+            int idi;
+
+            try
+            {
+                idi = Convert.ToInt32(id);
+            }
+
+            catch
+            {
+
+                read = "Fehlerhafte ID";
+                return read; // string
+
+            }
+
+            try
+            {
+                connection = new SQLiteConnection();
+                connection.ConnectionString = "Data Source=" + database;
+                connection.Open();
+
+
+
+                using (var command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "SELECT " + label + " FROM " + table + " WHERE ID = " + idi;
+
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+
+                            read = Convert.ToString(reader[0]);
+
+                        }
+                        reader.Close();
+                    }
+                }
+
+                connection.Close();
+                connection.Dispose();
+            }
+
+            catch (Exception ex)
+            {
+                read = "Fehler bei der Abfrage";
+            }
+            
+            return read; // string
+
+
+        }
+    }
+}
---
+++ qvpak_lib/testclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qvpak_lib
+{
+    public class testclass
+    {
+        public string callback()
+        {
+            string str = "Das ist C# .Net Code!";
+            return str;
+        }
+
+    }
+}
