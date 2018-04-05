0d1b6bde6e0599ccd86ffeb319683b8979f07d22
Blubbaxx <19249053+Blubbaxx@users.noreply.github.com>
2018-04-05 07:23:14 +0200

    add log

---
+++ qvpak_lib/log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace qvpak_lib
+{
+    public class log
+    {
+        public void run(string logfile, string content)
+        {
+
+            using (StreamWriter sw = File.AppendText(logfile))
+            {
+
+                sw.WriteLine(DateTime.Now.ToString() + " - " + content + ";");
+          
+            }
+
+        }
+    }
+}
